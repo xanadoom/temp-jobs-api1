df22db403a70f7e232732a94207c4b4e2b0db949
xanadoom <98607993+xanadoom@users.noreply.github.com>
2023-02-25 17:48:56 +0100

    a

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -35,8 +40,9 @@ app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send('Jobs api')
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
