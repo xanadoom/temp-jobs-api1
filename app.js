5e9cdca14e6faf1ee342e640f87558d2b88fa166
xanadoom <98607993+xanadoom@users.noreply.github.com>
2023-02-25 15:20:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get('/', (req, res) => {
+  '<h1>Jobs api</h1>'
+})
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
